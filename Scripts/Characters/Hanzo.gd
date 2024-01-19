63f74503c0cab4528dab55713f3004df308dce81
RayanSamaha <rayan.gamedev@gmail.com>
2024-01-19 21:53:47 +0200

    Added Hanzo multiple Shuriken throw attack

--- Scripts/Characters/Hanzo.gd
+++ Scripts/Characters/Hanzo.gd
@@ -5,8 +5,18 @@ var player
 
 #Teleport Properties
 @onready var teleportTimer = $TeleportCooldown
-var teleportRadius = 50
-var teleportTimerCooldown = 15
+const teleportRadius = 80
+const teleportTimerCooldown = 15
+
+#Shuriken Throw Properties
+@onready var shurikenSpawnPoint = $Muzzle
+@onready var shurikenThrowTimer = $SpecialAttackCooldown
+const shurikenThrowCooldown = 1
+const SHURIKEN = preload("res://Scenes/Items/Bullet.tscn")
+
+#Clones Cooldown
+@onready var clonesTimer = $ClonesCooldown
+const clonesCooldown = 30
 
 func _ready():
 	speed = 200
@@ -14,9 +24,13 @@ func _ready():
 	targetDistanceToPlayer = 80
 	
 func _process(delta):
+	print(shurikenThrowTimer.time_left)
 	if teleportTimer.is_stopped():
 		teleport()
+	if shurikenThrowTimer.is_stopped():
+		throwShuriken()
 
+#Teleport
 func teleport():
 	teleportTimer.start()
 	var newRandPos = position + Vector2(randi_range(-teleportRadius, teleportRadius), randi_range(-teleportRadius, teleportRadius))
@@ -29,3 +43,30 @@ func teleport():
 
 func _on_teleport_timeout():
 	teleportTimer.wait_time = teleportTimerCooldown
+	
+#Shuriken
+func throwShuriken():
+	shurikenThrowTimer.start()
+	var shuriken1 = SHURIKEN.instantiate()
+	var shuriken2 = SHURIKEN.instantiate()
+	var shuriken3 = SHURIKEN.instantiate()
+	
+	get_parent().get_parent().add_child(shuriken1)
+	get_parent().get_parent().add_child(shuriken2)
+	get_parent().get_parent().add_child(shuriken3)
+	
+	shuriken1.transform = shurikenSpawnPoint.global_transform
+	shuriken2.transform = shurikenSpawnPoint.global_transform
+	shuriken2.global_rotation_degrees = shurikenSpawnPoint.global_rotation_degrees + 30
+	shuriken3.transform = shurikenSpawnPoint.global_transform
+	shuriken3.global_rotation_degrees = shurikenSpawnPoint.global_rotation_degrees - 30
+
+func _on_special_attack_cooldown_timeout():
+	shurikenThrowTimer.wait_time = shurikenThrowCooldown
+
+#Create Clones
+func createClones():
+	pass
+
+func _on_clones_cooldown_timeout():
+	clonesTimer.wait_time = clonesCooldown
