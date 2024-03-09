20404738d00244559a713f6bedf29d7300190f28
RayanSamaha <rayan.gamedev@gmail.com>
2024-03-10 00:44:30 +0200

    Added samurai art and animations

--- Scripts/Characters/Samurai.gd
+++ Scripts/Characters/Samurai.gd
@@ -1,5 +1,6 @@
 extends Enemy
 
+@onready var animStateMachine = $AnimationTree.get("parameters/playback")
 @onready var attackCooldownTimer = $AttackCooldown
 @onready var attackCooldown = 0.2
 
@@ -16,7 +17,7 @@ func _process(delta):
 
 func _physics_process(delta):
 	if not player == null and state == State.CHASING:
-		look_at(player.global_position)
+		animStateMachine.travel("run")
 		var dir = (player.global_position - global_position).normalized()
 		move_and_collide(dir * speed * delta)
 		
@@ -27,8 +28,20 @@ func _physics_process(delta):
 		
 func attack():
 	if not player == null and attackCooldownTimer.is_stopped():
+		animStateMachine.travel("attack_1")
 		attackCooldownTimer.start()
 		player.takeDamage(1)
+		
+func takeDamage(damage):
+	animStateMachine.travel("get_hit")
+	hp -= damage
+	print(hp)
+	
+	if hp <= 0:
+		animStateMachine.travel("die")
 	
 func _on_attack_cooldown_timeout():
 	attackCooldownTimer.wait_time = attackCooldown
+	
+func destroySamurai():
+	queue_free()
