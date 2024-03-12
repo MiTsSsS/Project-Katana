3369f693f9bb750ef28b74a89a642dcca684097b
RayanSamaha <rayan.gamedev@gmail.com>
2024-03-13 00:16:05 +0200

    Finished Samurai animations

--- Scripts/Characters/Samurai.gd
+++ Scripts/Characters/Samurai.gd
@@ -1,10 +1,12 @@
 extends Enemy
 
-@onready var animStateMachine = $AnimationTree.get("parameters/playback")
+@onready var animStateMachine = $AnimationTree
 @onready var attackCooldownTimer = $AttackCooldown
 @onready var attackCooldown = 0.2
 
 var speed = 200
+var dead = false
+
 var player
 
 func _ready():
@@ -16,32 +18,46 @@ func _process(delta):
 		attack()
 
 func _physics_process(delta):
-	if not player == null and state == State.CHASING:
-		animStateMachine.travel("run")
+	if not player == null and state == State.CHASING and not dead:
+		animStateMachine["parameters/conditions/running"] = true
 		var dir = (player.global_position - global_position).normalized()
 		move_and_collide(dir * speed * delta)
 		
-	if not player == null and global_position.distance_to(player.global_position) < targetDistanceToPlayer:
+	if not player == null and global_position.distance_to(player.global_position) < targetDistanceToPlayer and not dead:
 		state = State.ATTACKING
+		animStateMachine["parameters/conditions/running"] = false
 	else:
 		state = State.CHASING
 		
 func attack():
 	if not player == null and attackCooldownTimer.is_stopped():
-		animStateMachine.travel("attack_1")
+		animStateMachine["parameters/playback"].travel("attack_1")
 		attackCooldownTimer.start()
-		player.takeDamage(1)
 		
 func takeDamage(damage):
-	animStateMachine.travel("get_hit")
+	animStateMachine["parameters/conditions/running"] = false
+	animStateMachine["parameters/conditions/attack_1"] = false
 	hp -= damage
-	print(hp)
 	
-	if hp <= 0:
-		animStateMachine.travel("die")
-	
+	if hp <= 0 and not dead:
+		dead = true
+		animStateMachine["parameters/conditions/died"] = true
+
 func _on_attack_cooldown_timeout():
 	attackCooldownTimer.wait_time = attackCooldown
-	
+	 
 func destroySamurai():
 	queue_free()
+
+func _on_first_attack_body_entered(body):
+	if body.is_in_group("player"):
+		var hitObj := body as Player
+		hitObj.takeDamage(15)
+
+func _on_second_attack_body_entered(body):
+	if body.is_in_group("player"):
+		var hitObj := body as Player
+		hitObj.takeDamage(15)
+
+func attackAnimationEnded():
+	animStateMachine["parameters/conditions/attack_1"] = false
