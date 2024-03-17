d4395c7cb3d83f5458cee4eaa1e4c1d3ced39e59
RayanSamaha <rayan.gamedev@gmail.com>
2024-03-17 23:32:04 +0200

    Fixed flipping of player character and Smaurai enemy

--- Scripts/Characters/Samurai.gd
+++ Scripts/Characters/Samurai.gd
@@ -1,5 +1,7 @@
 extends Enemy
 
+@onready var flipMarker:Marker2D = $FlipMarker
+
 @onready var animStateMachine = $AnimationTree
 @onready var attackCooldownTimer = $AttackCooldown
 @onready var attackCooldown = 0.2
@@ -21,8 +23,16 @@ func _physics_process(delta):
 	if not player == null and state == State.CHASING and not dead:
 		animStateMachine["parameters/conditions/running"] = true
 		var dir = (player.global_position - global_position).normalized()
-		move_and_collide(dir * speed * delta)
+		print(dir) 
+		if dir.x < 0:
+			flipMarker.scale = Vector2(-1, 1)
+			sprite.flip_h = true
+		else:
+			flipMarker.scale = Vector2(1, 1)
+			sprite.flip_h = false
 		
+		move_and_collide(dir * speed * delta)
+			
 	if not player == null and global_position.distance_to(player.global_position) < targetDistanceToPlayer and not dead:
 		state = State.ATTACKING
 		animStateMachine["parameters/conditions/running"] = false
@@ -38,7 +48,8 @@ func takeDamage(damage):
 	animStateMachine["parameters/conditions/running"] = false
 	animStateMachine["parameters/conditions/attack_1"] = false
 	hp -= damage
-	
+	print("Samurai HP: ")
+	print(hp)
 	if hp <= 0 and not dead:
 		dead = true
 		animStateMachine["parameters/conditions/died"] = true
