adf0af68bb748d5ec449494439f33ab98ad37f0d
RayanSamaha <rayan.gamedev@gmail.com>
2024-06-07 02:20:04 +0300

    Merchant shop items are now randomized when player enters Added Data tables plugin. Merchant items details are now displayed in UI.

--- Scripts/World/Merchant.gd
+++ Scripts/World/Merchant.gd
@@ -4,6 +4,32 @@ extends Interactable
 
 #Shop Functionatily
 
+func _ready():
+	var collection: Collection = Collection.new("res://Tables/ShopItems.tableCollection.res")
+	
+	if !collection.has_table("ShopItem"):
+		push_error("The table 'ShopItem' doesn't exist!")
+		return
+	 
+	var datatable: Datatable = collection.get_table("ShopItem")
+	 
+	if !datatable.has_item("Speed"):
+		push_error("The table 'ShopItem' doesn't have 'Speed' key!")
+		return
+	
+	var speed: Dictionary = datatable.get_item("Speed")	
+	var damage: Dictionary = datatable.get_item("Damage")
+	var dashCooldown: Dictionary = datatable.get_item("DashCooldown") 
+	var maxHp: Dictionary = datatable.get_item("MaxHp") 
+	var heal: Dictionary = datatable.get_item("Heal") 
+	
+	var allItems = [speed, damage, dashCooldown, maxHp, heal]
+	allItems.shuffle()
+	
+	var randomizedItems = [allItems[0], allItems[1], allItems[3]]
+
+	initItemsUi(randomizedItems)
+
 #Merchant Interact
 func _on_interact_body_entered(body:Node2D):
 	if body.is_in_group("player"):
@@ -20,3 +46,12 @@ func _on_interact_body_exited(body:Node2D):
 
 func interactedWith():
 	shopUI.visible = true
+
+func initItemsUi(randomizedItems):
+	var itemsHolder:HBoxContainer = $CanvasLayer/PanelContainer/ShopItems_HB
+
+	for n in 3:
+		var shopItemUi = itemsHolder.get_children()[n]
+		shopItemUi.get_node("ItemName").text = randomizedItems[n]["itemname"]
+		shopItemUi.get_node("ItemDescription").text = "Increase by " + str(randomizedItems[n]["itemvalue"])
+		shopItemUi.get_node("Button").text = "Buy for " + str(randomizedItems[n]["cost"])
